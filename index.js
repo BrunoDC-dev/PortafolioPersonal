8e0a63a57e232d38231ae4d60aa25e4d43ef9bd5
bruno <brunodecruz.05@gmail.com>
2023-05-10 23:03:07 -0300

    v1.0.2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/submit-form', (req, res) => {
+app.post('/api/submit-form', (req, res) => {
     const name = req.body.name;
     const email = req.body.email;
     const subject = req.body.subject;
@@ -38,4 +38,5 @@ app.post('/submit-form', (req, res) => {
       }
     });
   });
+  app.listen(3000, () => console.log('Server started on port 3000'));
   
